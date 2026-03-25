3988d5655f297bb9a549575dc96e91da87b5d21c
Shoaib Taimur <mitashoaib@gmail.com>
2026-03-26 02:02:57 +0600

    Added category

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,10 +1,50 @@
+import { Brush, Code, Film, GraduationCap, Megaphone } from "lucide-react";
 
 const Category = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="gradient-primary px-10 py-10">
+      <h1 className="inter text-center font-bold text-[20px] md:text-[30px]">Browse by Category</h1>
+      <p className="text-center inter opacity-60">Find the perfect skill in your area of interest</p>
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-10 mt-10">
+        <div className="p-6 bg-white flex flex-col gap-2 justify-center items-center shadow rounded-xl">
+          <div className="bg-linear-to-br from-pink-500 to-rose-500 h-14 w-14 rounded-2xl flex items-center justify-center">
+            <Brush size={30} color="white" />
+          </div>
+          <h1 className="inter text-[14px] font-bold">Design</h1>
+          <p className="inter text-[12px] opacity-60">234 skills</p>
         </div>
-    );
+        <div className="p-6 bg-white flex flex-col gap-2 justify-center items-center shadow rounded-xl">
+          <div className="bg-linear-to-br from-blue-500 to-cyan-500 h-14 w-14 rounded-2xl flex items-center justify-center">
+            <Code size={30} color="white" />
+          </div>
+          <h1 className="inter text-[14px] font-bold">Programming</h1>
+          <p className="inter text-[12px] opacity-60">234 skills</p>
+        </div>
+        <div className="p-6 bg-white flex flex-col gap-2 justify-center items-center shadow rounded-xl">
+          <div className="bg-linear-to-br from-orange-500 to-amber-500 h-14 w-14 rounded-2xl flex items-center justify-center">
+            <Megaphone size={30} color="white" />
+          </div>
+          <h1 className="inter text-[14px] font-bold">Marketing</h1>
+          <p className="inter text-[12px] opacity-60">234 skills</p>
+        </div>
+        <div className="p-6 bg-white flex flex-col gap-2 justify-center items-center shadow rounded-xl">
+          <div className="bg-linear-to-br from-green-500 to-emerald-500 h-14 w-14 rounded-2xl flex items-center justify-center">
+            <GraduationCap size={30} color="white" />
+          </div>
+          <h1 className="inter text-[14px] font-bold">Teaching</h1>
+          <p className="inter text-[12px] opacity-60">234 skills</p>
+        </div>
+        <div className="p-6 bg-white flex flex-col gap-2 justify-center items-center shadow rounded-xl">
+          <div className="bg-linear-to-br from-purple-500 to-violet-500 h-14 w-14 rounded-2xl flex items-center justify-center">
+            <Film size={30} color="white" />
+          </div>
+          <h1 className="inter text-[14px] font-bold">Video Editing</h1>
+          <p className="inter text-[12px] opacity-60">234 skills</p>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Category;
