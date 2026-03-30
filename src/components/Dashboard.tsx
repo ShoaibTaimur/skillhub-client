22b7642a6c077e76c5bbceaf19bae21c6fa53e55
Shoaib Taimur <mitashoaib@gmail.com>
2026-03-31 02:02:50 +0600

    Added dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,96 @@
+import { AuthContext } from "@/Context/AuthContext";
+import { Eye, Heart, Newspaper } from "lucide-react";
+import { useContext, useEffect, useState } from "react";
+import SkillTab from "./SkillTab";
+import Swal from "sweetalert2";
+
+type Skill = {
+  _id: string;
+  title: string;
+  category: string;
+  price: string;
+  description: string;
+  name: string;
+};
 
 const Dashboard = () => {
-    return (
-        <div>
-            dashboard
+  const info = useContext(AuthContext);
+  const user = info?.user;
+  const name = user?.displayName;
+  const [skills, setSkills] = useState<Skill[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await fetch(`https://skillhub-server-bice.vercel.app/skills/${name}`);
+      const data = await res.json();
+      setSkills(data);
+    };
+    loadData();
+  }, [name]);
+  const handleDelete = (id: string) => {
+    fetch(`https://skillhub-server-bice.vercel.app/skills/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged || data.deletedCount > 0) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your Skill has been deleted.",
+            icon: "success",
+          });
+
+          const remain = skills.filter((skill) => skill._id !== id);
+          setSkills(remain);
+        }
+      });
+  };
+  return (
+    <div className="gradiant-primary px-4 sm:px-10">
+      <h1 className="inter font-bold text-[25px] md:text-[30px]">Dashboard</h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mb-3">
+        <div className="flex items-center gap-12 border border-blue-100 rounded-lg p-4 ">
+          <div className="w-[10%]">
+            <div className="p-2 sm:p-3 rounded-lg w-fit bg-blue-100">
+              <Newspaper />
+            </div>
+          </div>
+          <div className="w-[90%]">
+            <h1 className="inter text-[18px] md:text-[24px] font-bold">{skills.length}</h1>
+            <p className="inter text-[12px] md:text-[15px] opacity-60">Total Posts</p>
+          </div>
         </div>
-    );
+        <div className="flex items-center gap-12 border border-blue-100 rounded-lg p-4 ">
+          <div className="w-[10%]">
+            <div className="p-2 sm:p-3 rounded-lg w-fit bg-blue-100">
+              <Heart />
+            </div>
+          </div>
+          <div className="w-[90%]">
+            <h1 className="inter text-[18px] md:text-[24px] font-bold">{skills.length}</h1>
+            <p className="inter text-[12px] md:text-[15px] opacity-60">Total Like</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-6 md:gap-12 border border-blue-100 rounded-lg p-4 col-span-2 md:col-span-1">
+          <div className="w-[10%]">
+            <div className="p-2 sm:p-3 rounded-lg w-fit bg-blue-100">
+              <Eye />
+            </div>
+          </div>
+          <div className="w-[90%]">
+            <h1 className="inter text-[18px] md:text-[24px] font-bold">{skills.length}</h1>
+            <p className="inter text-[12px] md:text-[15px] opacity-60">Total Views</p>
+          </div>
+        </div>
+      </div>
+      <h1 className="inter font-bold text-[16px] md:text-[22px]">My skills</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+        {skills.map((skill) => (
+          <SkillTab key={skill._id} skill={skill} handleDelete={handleDelete}></SkillTab>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/SkillTab.tsx
@@ -0,0 +1,72 @@
+import { Button } from "./ui/button";
+import { Eye, Heart } from "lucide-react";
+
+type Skill = {
+  _id: string;
+  title: string;
+  category: string;
+  price: string;
+  description: string;
+  name: string;
+};
+
+type SkillTabProps = {
+  skill: Skill;
+  handleDelete: (id: string) => void;
+};
+
+const SkillTab = ({ skill, handleDelete }: SkillTabProps) => {
+  const title = skill.title;
+  const name = skill.name;
+  const category = skill.category;
+  const price = skill.price;
+  const id = skill._id;
+
+  const handleEdit = () => {};
+  
+  return (
+    <div className="bg-white p-4 rounded-3xl shadow flex flex-col gap-5">
+      <div className="mt-3 h-[70%]">
+        <div className="bg-[#ede0fd98] py-0.75 px-3 rounded-2xl w-fit flex items-center mb-2">
+          <p className="gradient-text font-bold text-[12px]">{category}</p>
+        </div>
+        <h1 className="inter font-medium text-[16px] mb-1">{title}</h1>
+        <p className="opacity-80">
+          By- <span className="gradient-text font-extrabold">{name}</span>
+        </p>
+      </div>
+      <div className="flex justify-between items-center h-[15%]">
+        <p className="gradient-text inter font-bold">${price}</p>
+        <div className="flex gap-2">
+          <div className="flex opacity-60 gap-1">
+            <Heart />
+            <p>123</p>
+          </div>
+          <div className="flex opacity-60 gap-1">
+            <Eye />
+            <p>189</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <Button
+          onClick={() => handleEdit()}
+          variant="login"
+          className="w-[50%] h-10"
+          disabled
+        >
+          Edit Deatils
+        </Button>
+        <Button
+          onClick={() => handleDelete(id)}
+          variant="destructive"
+          className="w-[50%] h-10"
+        >
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SkillTab;
